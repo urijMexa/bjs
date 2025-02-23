c41d2504dc468625da7a27265e83b71cf255cbfb
Yuri Blinnikov <blinnikovu5@gmail.com>
2025-02-23 12:53:32 +0800

    55

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -87,3 +87,79 @@ picknick.state = 10;
 console.log(picknick.state); // 10
 picknick.fix();
 console.log(picknick.state); // 15
+
+
+class Library {
+    constructor(name) {
+        this.name = name; // Название библиотеки
+        this.books = []; // Хранилище книг (пустой массив)
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book); // Добавляем книгу, если её состояние > 30
+        }
+    }
+
+    findBookBy(type, value) {
+        return this.books.find(book => book[type] === value) || null;
+        // Возвращаем найденную книгу или null, если книга не найдена
+    }
+
+    giveBookByName(bookName) {
+        const index = this.books.findIndex(book => book.name === bookName);
+        if (index !== -1) {
+            return this.books.splice(index, 1)[0];
+        }
+        return null;
+    }
+}
+
+// Создаем библиотеку
+const library = new Library("Библиотека имени Ленина");
+
+// Добавляем книги и журналы
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+
+console.log(library.findBookBy("name", "Властелин колец"));
+console.log(library.findBookBy("releaseDate", 1924).name);
+
+
+console.log("Количество книг до выдачи: " + library.books.length);
+
+
+const givenBook = library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length);
+
+
+if (givenBook) {
+    givenBook.state = 20;
+    console.log("Состояние выданной книги: " + givenBook.state);
+}
+
+if (givenBook) {
+    givenBook.fix();
+    console.log("Состояние восстановленной книги: " + givenBook.state);
+}
+
+
+library.addBook(givenBook);
+console.log("Количество книг после попытки добавления: " + library.books.length);
